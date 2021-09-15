616616397fd9024ea909442b54dc5ec163701f7e
kahhorjonov <akbarjon3028@gmail.com>
2021-09-15 14:18:27 +0500

    adminPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return ();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-message">
+          <p>Login va Parolingizni kiriting</p>
+        </div>
+        <div className="App-input">
+          <input className="input" placeholder="login" type="text"></input>
+          <input
+            className="input"
+            placeholder="password"
+            type="password"
+          ></input>
+        </div>
+        <div className="App-footer">
+          <button className="btn btn-submit bg-light mt-4">Submit</button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
