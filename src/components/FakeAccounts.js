367e81b73c49b890527cf02f09a88f33901912f1
kahhorjonov <akbarjon3028@gmail.com>
2021-09-15 16:28:47 +0500

    success

---
+++ src/components/FakeAccounts.js
@@ -0,0 +1,10 @@
+const account1 = {
+  login: "kahhorjonov",
+  pin: 1204,
+};
+
+const accounts = [account1];
+
+export function getAccounts() {
+  return accounts;
+}
--- src/components/starterPage.jsx
+++ src/components/starterPage.jsx
@@ -2,8 +2,31 @@ import React, { Component } from "react";
 import "./StarterPage.css";
 
 import "bootstrap/dist/css/bootstrap.css";
+import { getAccounts } from "./FakeAccounts";
 
 class StarterPage extends Component {
+  state = {
+    accounts: getAccounts(),
+  };
+
+  handleCheck = () => {
+    const accounts = [...this.state.accounts];
+    const currentAccountLogin = document.querySelector(".login").value;
+    const currentAccountPassword = document.querySelector(".password").value;
+    const adminPage = document.querySelector(".adminPage");
+    const appPage = document.querySelector(".App");
+    let currentAccount;
+
+    currentAccount = accounts.find((acc) => acc.login === currentAccountLogin);
+
+    // console.log(currentAccount);
+
+    if (currentAccount.pin === Number(currentAccountPassword)) {
+      adminPage.classList.toggle("hidden");
+      appPage.classList.add("hidden");
+    }
+  };
+
   render() {
     return (
       <div>
@@ -13,17 +36,30 @@ class StarterPage extends Component {
               <p>Login va Parolingizni kiriting</p>
             </div>
             <div className="App-input">
-              <input className="input" placeholder="login" type="text"></input>
               <input
-                className="input"
+                className="input login"
+                placeholder="login"
+                type="text"
+              ></input>
+              <input
+                className="input password"
                 placeholder="password"
                 type="password"
               ></input>
             </div>
             <div className="App-footer">
-              <button className="btn btn-submit bg-light mt-4">Submit</button>
+              <button
+                onClick={() => this.handleCheck(this.accounts)}
+                className="btn btn-submit bg-light mt-4"
+              >
+                Submit
+              </button>
             </div>
           </header>
+        </div>
+
+        <div className="adminPage hidden">
+          <p>Hello Friend</p>
         </div>
       </div>
     );
